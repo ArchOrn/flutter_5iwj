3f5e5489fd6a9a9f3399225ffc32d60904304dea
Raphaël Kiffer <kiffer.raphael@gmail.com>
2023-11-02 10:16:29 +0100

    FutureBuilder rewrite for posts list

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,90 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_5iwj/core/errors/network_error.dart';
-import 'package:flutter_5iwj/core/models/post.dart';
 import 'package:flutter_5iwj/core/services/api_services.dart';
 import 'package:flutter_5iwj/home/post_item.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  bool _loading = true;
-  Error? _error;
-  List<Post> _posts = [];
-
-  @override
-  void initState() {
-    super.initState();
-
-    ApiServices.getPosts().then((posts) {
-      setState(() {
-        _posts = posts;
-        _loading = false;
-        _error = null;
-      });
-    }).catchError((error) {
-      setState(() {
-        _posts = [];
-        _loading = false;
-        _error = error;
-      });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Builder(builder: (context) {
-          if (_loading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+        child: FutureBuilder(
+            future: ApiServices.getPosts(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
 
-          if (_error != null) {
-            return Center(
-              child: Text(
-                _error is NetworkError ? 'Network error' : 'Error',
-                style: const TextStyle(color: Colors.red),
-              ),
-            );
-          }
-
-          if (_posts.isEmpty) {
-            return const Center(
-              child: Text('No posts available'),
-            );
-          }
-
-          return ListView.separated(
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(10),
-                child: PostItem(post: _posts[index]),
-              );
-            },
-            separatorBuilder: (context, index) {
-              return Column(
-                children: [
-                  const SizedBox(height: 5),
-                  Container(
-                    width: double.infinity,
-                    height: 1,
-                    color: Colors.blue,
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    snapshot.error is NetworkError ? 'Network error' : 'Error',
+                    style: const TextStyle(color: Colors.red),
                   ),
-                  const SizedBox(height: 5),
-                ],
+                );
+              }
+
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: Text('No posts available'),
+                );
+              }
+
+              final posts = snapshot.data!;
+              return ListView.separated(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: PostItem(post: posts[index]),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      const SizedBox(height: 5),
+                      Container(
+                        width: double.infinity,
+                        height: 1,
+                        color: Colors.blue,
+                      ),
+                      const SizedBox(height: 5),
+                    ],
+                  );
+                },
+                itemCount: posts.length,
               );
-            },
-            itemCount: _posts.length,
-          );
-        }),
+            }),
       ),
     );
   }
